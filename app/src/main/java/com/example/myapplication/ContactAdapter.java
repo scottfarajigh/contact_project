0bac9b9aea7146128deb2e7130823af1d29cd853
scottfarajigh <128868296+scottfarajigh@users.noreply.github.com>
2023-04-25 05:26:07 -0700

    Habib (#2)
    * charge_resources
    
    * contact_empale_added
    
    * app_is_clickable
    
    * app_is_lived
    
    * edit_contatc_feautere
    
    * delete_contact_feautre

---
+++ app/src/main/java/com/example/myapplication/ContactAdapter.java
@@ -0,0 +1,107 @@
+package com.example.myapplication;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactViewHolder> {
+    ArrayList<String> contacts = new ArrayList<>();
+    ItemViewListener itemViewListener;
+    private static final String TAG = "ContactAdapter";
+    ContactAdapter(ItemViewListener itemViewListener){
+        this.itemViewListener = itemViewListener;
+        contacts.add("Ali alipour");
+        contacts.add("Ali bahmani") ;
+        contacts.add("Bardia rezari")  ;
+        contacts.add("Catlin jpour")  ;
+        contacts.add("Dariosh akbari")  ;
+        contacts.add("Elham hosseini");
+        contacts.add("Farid rezaei");
+        contacts.add("Fardin alipour");
+        contacts.add("Gloshifte ahmadi") ;
+        contacts.add("Habib faraji") ;
+        contacts.add("Hadi faraji") ;
+        contacts.add("Mailad atatei") ;
+        contacts.add("Matin sinaei");
+        contacts.add("Mohsen hasani");
+        contacts.add("Karim zandi");
+        contacts.add("Korosh gandi");
+        contacts.add("Leyla karimi") ;
+        contacts.add("Fershte sereshte");
+        contacts.add("Azin miladi") ;
+        contacts.add("Mohammad bibak") ;
+
+    }
+    public void addContact(String fullName){
+        contacts.add(0,fullName);
+        notifyItemInserted(0);
+    }
+
+    public void changeContact(String fullname , int position){
+        contacts.set(position,fullname);
+        notifyItemChanged(position);
+    }
+
+    @NonNull
+    @Override
+    public ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.i(TAG, "onCreateViewHolder: ");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contact,parent,false);
+        return new ContactViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactViewHolder holder, int position) {
+        Log.i(TAG, "onBindViewHolder: موقعیت ==> " + position);
+        holder.bindContact(contacts.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return contacts.size();
+    }
+
+    public class ContactViewHolder extends RecyclerView.ViewHolder{
+        TextView firstCharacterTv;
+        TextView contactFullNameTv;
+        public ContactViewHolder(@NonNull View itemView) {
+            super(itemView);
+            firstCharacterTv=itemView.findViewById(R.id.firstCharacterTv);
+            contactFullNameTv=itemView.findViewById(R.id.contactFullNamreTv);
+        }
+
+        public void bindContact(String fullName){
+            firstCharacterTv.setText(fullName.substring(0,1));
+            contactFullNameTv.setText(fullName);
+            contactFullNameTv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    itemViewListener.onItemClick(fullName,getAdapterPosition());
+                    Toast.makeText(view.getContext(), fullName, Toast.LENGTH_SHORT).show();
+                }
+            });
+            contactFullNameTv.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    contacts.remove(getAdapterPosition());
+                    notifyItemRemoved(getAdapterPosition());
+                    Toast.makeText(view.getContext(), fullName + " Is Deleted !", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            });
+        }
+    }
+
+    public interface ItemViewListener{
+        public void onItemClick(String fullName , int position);
+    }
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -15,15 +17,54 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ContactAdapter.ItemViewListener {
+    int editingItemPosition;
+    ContactAdapter adapter;
+    ImageView addContactBtn;
+    EditText addContactFullName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView =findViewById(R.id.newMainRv);
+        adapter = new ContactAdapter(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
+        recyclerView.setAdapter(adapter);
+
+        addContactFullName =findViewById(R.id.edAddContact);
+        addContactBtn=findViewById(R.id.addBtn);
+        addContactBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (addContactFullName.length() > 0){
+                    if (editingItemPosition  > -1){
+                        adapter.changeContact(addContactFullName.getText().toString(),editingItemPosition);
+                        editingItemPosition = -1;
+                        addContactBtn.setImageResource(R.drawable.ic_baseline_person_add_alt_1_24);
+                    }else{
+                        adapter.addContact(addContactFullName.getText().toString());
+                        recyclerView.smoothScrollToPosition(0);
+                    }
+
+                }
+                addContactFullName.setText("");
+            }
+        });
+
+    }
+
+    @Override
+    public void onItemClick(String fullName, int position) {
+        addContactFullName.setText(fullName);
+        editingItemPosition=position;
+        addContactBtn.setImageResource(R.drawable.ic_baseline_done_24);
 
     }
 }
